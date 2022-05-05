2fcbbc2e5547dfdfc4e0441875f56afc7c01e0c6
Karpukhin-EV <evkarpukhin@mail.ru>
2022-05-05 13:54:36 +0300

    First commit

---
+++ src/main/java/pro/sky/kkp_2/Kkp2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.kkp_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kkp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kkp2Application.class, args);
+    }
+
+}
